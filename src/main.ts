7a0e136450d7ef8b9ecab09a4d203b09488eeec9
gregmaax <greg.d.972@gmail.com>
2023-03-29 23:09:59 +0200

    styling

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,19 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter'
+import './style.css';
+import { placeholder } from '.';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <div class="container">
+    <div class="blocTitre">
+      <h1>What can I cook ?</h1>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <div class="blocInput">
+      <input type="text" id="inputDataId" placeholder="Entrez vos ingrédients...">
+    <button id="validerId">Valider</button>
+    </div>
+    <div class="blocResultat">
+      <span id="resultatId">${placeholder}</span>
+    </div>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+//setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
